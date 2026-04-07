45d51e4a0999584fa0dae502f3eeacb081a3bed5
Adithya P Kashyap <adithyapkashyap@gmai.com>
2026-04-07 14:27:54 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 import './styles/base/variables.css';
 import './styles/base/global.css';
 
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </AppProvider>
   </React.StrictMode>
 );
